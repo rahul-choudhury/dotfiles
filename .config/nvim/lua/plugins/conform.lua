47c5feaac03cdf614aaefe83908552c0dd035ac0
Rahul Choudhury <rchoudhury63@gmail.com>
2024-02-21 22:24:13 +0530

    feat(nvim): add filetype spec to conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,18 +2,20 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      sh = { "shfmt" },
-      zsh = { "shfmt" },
+      c = { "clang_format" },
+      cpp = { "clang_format" },
       lua = { "stylua" },
       html = { "prettierd" },
       css = { "prettierd" },
+      json = { "prettierd" },
+      jsonc = { "prettierd" },
+      yaml = { "prettierd" },
       javascript = { "prettierd" },
       typescript = { "prettierd" },
       javascriptreact = { "prettierd" },
       typescriptreact = { "prettierd" },
     },
     format_on_save = {
-      timeout_ms = 500,
       lsp_fallback = true,
     },
   },
