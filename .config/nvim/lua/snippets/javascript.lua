3e3981c939bbbbea58a69ad82e387e10041b1a43
Rahul Choudhury <rchoudhury63@gmail.com>
2024-03-22 23:20:47 +0530

    feat(nvim): add snippets

--- .config/nvim/lua/snippets/javascript.lua
+++ .config/nvim/lua/snippets/javascript.lua
@@ -2,7 +2,6 @@ local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
-local c = ls.choice_node
 
 return {
   -- console log
@@ -26,15 +25,6 @@ return {
     ]], {i(1, "name"), i(2, "params"), i(3)}
   )),
 
-  -- arrow function
-  s("af", fmt(
-    [[
-    const {} = ({}) => {{
-      {}
-    }}
-    ]], {i(1, "name"), i(2, "params"), i(3)}
-  )),
-
   -- try...catch block
   s("tc", fmt(
     [[
@@ -46,21 +36,12 @@ return {
     ]], {i(2), i(1, "error"), i(3)}
   )),
 
-  -- default function export
-  s("edf", fmt(
+  -- for...of loop
+  s("forof", fmt(
     [[
-    export default function {}({}) {{
+    for ({} of {}) {{
       {}
     }}
-    ]], {i(1, "name"), i(2, "params"), i(3)}
-  )),
-
-  -- named function export
-  s("ef", fmt(
-    [[
-    export function {}({}) {{
-      {}
-    }}
-    ]], {i(1, "name"), i(2, "params"), i(3)}
-  )),
+    ]], {i(1, "variable"), i(2, "iterable"), i(3)}
+  ))
 }
--- .config/nvim/lua/snippets/javascriptreact.lua
+++ .config/nvim/lua/snippets/javascriptreact.lua
@@ -1,18 +1,19 @@
 local ls = require("luasnip")
 local s = ls.snippet
+local t = ls.text_node
 local i = ls.insert_node
 local extras = require("luasnip.extras")
 local l = extras.lambda
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
-  -- jsx self closing tag
-  s("jc", fmt("<{} />", {i(1, "Component")})),
+  -- import
+  s("imp", {t('import * as React from "react"')}),
 
   -- useState
   s(
     "us",
-    fmt("const [{}, set{setter}] = useState({})", {
+    fmt("const [{}, set{setter}] = React.useState({})", {
       i(1, "state"),
       i(2),
       setter = l(l._1:sub(1, 1):upper() .. l._1:sub(2, -1), 1)
