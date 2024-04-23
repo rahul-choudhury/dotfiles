5404fc8d6aea8a2bc396618937c4616cc15d80da
Rahul Choudhury <rchoudhury63@gmail.com>
2024-04-23 09:41:42 +0530

    refactor: neovim options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -3,16 +3,26 @@ vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.list = true
-vim.opt.wrap = false
+
+vim.opt.breakindent = true
+
 vim.opt.undofile = true
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.updatetime = 100
-vim.opt.termguicolors = true
+
+vim.opt.updatetime = 250
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+
+vim.opt.inccommand = "split"
+
+vim.opt.hlsearch = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 vim.opt.signcolumn = "yes"
