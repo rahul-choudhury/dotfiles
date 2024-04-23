38b6fafe251da4b055e0cbb3cb50c0c876b83094
Rahul Choudhury <rchoudhury63@gmail.com>
2024-04-23 08:31:51 +0530

    feat: add telescope ui-select plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-ui-select.nvim",
     {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
@@ -13,27 +14,44 @@ return {
   },
   config = function()
     require("telescope").setup({
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = {
-          preview_width = 0.5,
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
         },
       },
     })
 
-    pcall(require("telescope").load_extension("fzf"))
-
-    local nmap = function(keys, func)
-      vim.keymap.set("n", keys, func)
-    end
+    pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "ui-select")
 
     local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags)
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps)
+    vim.keymap.set("n", "<leader>sf", builtin.find_files)
+    vim.keymap.set("n", "<leader>sw", builtin.grep_string)
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep)
+    vim.keymap.set("n", "<leader>sd", builtin.diagnostics)
+    vim.keymap.set("n", "<leader>sr", builtin.resume)
+    vim.keymap.set("n", "<leader>s.", builtin.oldfiles)
+    vim.keymap.set("n", "<leader><leader>", builtin.buffers)
 
-    nmap("<leader>sf", builtin.find_files)
-    nmap("<leader>sw", builtin.grep_string)
-    nmap("<leader>sg", builtin.live_grep)
-    nmap("<leader>sh", builtin.help_tags)
-    nmap("<leader>?", builtin.oldfiles)
-    nmap("<leader><space>", builtin.buffers)
+    vim.keymap.set("n", "<leader>/", function()
+      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+        winblend = 10,
+        previewer = false,
+      }))
+    end)
+
+    vim.keymap.set("n", "<leader>s/", function()
+      builtin.live_grep({
+        grep_open_files = true,
+        prompt_title = "Live Grep in Open Files",
+      })
+    end)
+
+    -- Shortcut for searching Neovim configuration files
+    vim.keymap.set("n", "<leader>sn", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end)
   end,
 }
