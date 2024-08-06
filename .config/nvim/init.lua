026c4564b8a52bfaadf91c1714ddce31c927c344
Rahul Choudhury <rchoudhury63@gmail.com>
2024-08-07 00:27:44 +0530

    move nvim opts to separate file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,6 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.wrap = false
-vim.opt.undofile = true
-vim.opt.hlsearch = false
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 100
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.pumheight = 15
+require("opts")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
@@ -120,5 +100,5 @@ require("lazy").setup({
   { "folke/ts-comments.nvim", opts = {} },
   { "kylechui/nvim-surround", opts = {} },
 
-  { import = "plugins" },
+  { import = "lsp" },
 })
---
+++ .config/nvim/lua/opts.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 100
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.pumheight = 15
